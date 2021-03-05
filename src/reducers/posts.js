20eb4a153362719305b27f5520b1053062bf2f8e
hirajgajera <gajerahiraj@gmail.com>
2021-03-05 23:49:34 +0530

    first commit

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,6 +1,6 @@
 import {FETCH_ALL, CREATE, UPDATE, DELETE} from '../constants/actionType.js'
 
-export default (posts = [], action) => {
+export default function (posts = [], action) {
     switch(action.type){
         case FETCH_ALL: 
             return action.payload;
